21b02cfb9054d8c29ddbb21340c14bf904b84b07
lukazko <lukaz.kubik@gmail.com>
2021-09-06 17:07:50 +0200

    testy2

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
 from player import Player
 from team import Team
 
-def compare():
-    pass
+def compare(team1, team2):
+    rating1 = team1.get_overall_rating()
+    rating2 = team2.get_overall_rating()
+    
+    if rating1 > rating2:
+        return 'Výhrála ' + team1.name
+    elif rating1 < rating2:
+        return 'Vyhrála' + team2.name
+    else:
+        return 'Remíza!'
+
 
 player1 = Player('Leo Messi', 90)
 player2 = Player('Cristiano Ronaldo', 90)
 #print(player1.get_name())
 #print(player1.get_overall_rating())
 
-sparta = Team([player1.get_name(), player2.get_name()])
-print(sparta.get_players())
+sparta = Team("Sparta", player1, player2)
+##print(sparta.get_striker())
+##print(sparta.get_goalkeeper())
+##print(sparta.get_players())
+##print(sparta.get_overall_rating())
+
+player3 = Player('Robin Van Persie', 90)
+player4 = Player('David Beckham', 90)
+slavie = Team('Slavie', player3, player4)
+
+print(compare(sparta,slavie))
--- team.py
+++ team.py
@@ -1,6 +1,19 @@
 class Team:
-    def __init__(self, players):
-        self.players = players
+    def __init__(self, name, striker, goalkeeper):
+        self.striker = striker
+        self.goalkeeper = goalkeeper
+        self.name = name
+
+    def get_striker(self):
+        return self.striker.get_name()
+
+    def get_goalkeeper(self):
+        return self.goalkeeper.get_name()
 
     def get_players(self):
-        return self.players
+        team = {"striker": self.striker.get_name(), "goalkeeper": self.goalkeeper.get_name()}
+        return team.values()
+
+    def get_overall_rating(self):
+        rating = self.striker.get_overall_rating() + self.goalkeeper.get_overall_rating()
+        return rating     
